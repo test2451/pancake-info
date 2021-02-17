8a70375d2f5300fffc3d0244cc57b89316b9ecce
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-17 16:58:22 +0530

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import { Flex } from 'rebass'
 import Link from '../Link'
 
 const links = [
-  { url: 'https://pancakeswap.finance', text: 'About' },
-  { url: 'https://docs.pancakeswap.finance', text: 'Docs' },
-  { url: 'https://github.com/pancakeswap/pancakeswap-info', text: 'Code' },
+  { url: 'https://pieswap.org', text: 'About' },
+  { url: 'https://docs.pieswap.org', text: 'Docs' },
+  { url: 'https://github.com/pieswap/pieswap-info', text: 'Code' },
 ]
 
 const FooterLink = ({ children, ...rest }) => (
